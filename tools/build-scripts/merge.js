a17902e8d073d2087c4efe1dc57525b54a4d91e5
Thomas Pickard <tom@sliips.com>
2018-03-19 19:12:42 +0000

    scripts

---
+++ tools/build-scripts/merge.js
@@ -0,0 +1,47 @@
+
+var execSync = require('child_process').execSync;
+var exec = require('child_process').exec;
+const { spawn } = require('child_process');
+const readline = require('readline');
+
+let mergePackages = require("@userfrosting/merge-package-dependencies");
+let template = require('./package.json');
+
+
+
+// move the base package
+console.log('moving the base package');
+
+console.log( execSync("rm -rf workdir && mkdir workdir && cp -r ./node_modules/react-base/ workdir").toString() );
+
+
+
+// do the merge of the package.json
+console.log('Starting merge process');
+
+let pkgPaths = [
+    "./node_modules/react-base/"
+];
+
+console.log( execSync("rm workdir/package.json").toString() );
+let result = mergePackages.yarn(template, pkgPaths, "./workdir/");
+
+
+
+// install the dependencies
+console.log('running yarn install');
+
+console.log( execSync("pushd ./workdir && yarn install").toString() );
+
+
+
+// move our source code to the working directory
+console.log('Starting sync');
+
+console.log( execSync("rm -rf workdir/src/content").toString() );
+// we execute this syncronously
+//exec("yarn sync-files --watch src/content workdir/src/content");
+
+console.log( execSync("cp -r src/content workdir/src/content").toString() );
+
+console.log('done!');
---
+++ tools/build-scripts/mergeAndRunLocal.js
@@ -0,0 +1,123 @@
+
+var execSync = require('child_process').execSync;
+var exec = require('child_process').exec;
+const { spawn } = require('child_process');
+const readline = require('readline');
+
+let mergePackages = require("@userfrosting/merge-package-dependencies");
+let template = require('./package.json');
+
+
+
+// move the base package
+console.log('moving the base package');
+
+console.log( execSync("rm -rf workdir && mkdir workdir && cp -r ./node_modules/react-base/ workdir").toString() );
+
+
+
+// do the merge of the package.json
+console.log('Starting merge process');
+
+let pkgPaths = [
+    "./node_modules/react-base/"
+];
+
+console.log( execSync("rm workdir/package.json").toString() );
+let result = mergePackages.yarn(template, pkgPaths, "./workdir/");
+
+
+
+// install the dependencies
+console.log('running yarn install');
+
+console.log( execSync("pushd ./workdir && yarn install").toString() );
+
+
+
+// move our source code to the working directory
+console.log('Starting sync');
+
+console.log( execSync("rm -rf workdir/src/content").toString() );
+// we execute this syncronously
+//exec("yarn sync-files --watch src/content workdir/src/content");
+
+const sync = spawn('yarn', ['sync-files', '--watch', 'src/content', 'workdir/src/content']);
+
+sync.stdout.on('data', (data) => {
+  console.log(`${data}`);
+});
+
+sync.stderr.on('data', (data) => {
+  console.log(`${data}`);
+});
+
+sync.on('close', (code) => {
+  console.log(`sync process exited with code ${code}`);
+});
+
+
+//we pause for a bit to make sure the sync is done
+console.log('sleeping!');
+console.log( execSync("sleep 15").toString() );
+console.log('woke up!');
+
+// run the build
+console.log('Running build command');
+
+
+const build = spawn('yarn', ['build:local'], {
+  cwd: process.cwd()+'/workdir'
+});
+
+build.stdout.on('data', (data) => {
+  console.log(`${data}`);
+});
+
+build.stderr.on('data', (data) => {
+  console.log(`${data}`);
+});
+
+build.on('close', (code) => {
+  console.log(`The build process exited with ${code}`);
+
+  if(code === 0 || code === '0') {
+
+
+
+
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+
+      // run the start command
+      console.log('Running start command');
+
+      const start = spawn('yarn', ['start:local'], {
+        cwd: process.cwd()+'/workdir'
+      });
+
+      start.stdout.on('data', (data) => {
+        console.log(`${data}`);
+      });
+
+      start.stderr.on('data', (data) => {
+        console.log(`${data}`);
+      });
+
+      start.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+      });
+
+      rl.on('line', (input) => {
+        console.log(`Received: ${input} - passing to nodemon`);
+
+        start.stdin.write(input);
+      });
+
+
+
+  }
+});
+
