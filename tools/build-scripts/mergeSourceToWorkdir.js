b58a27ad9c132114b271c553084f4ac63b5bec81
Thomas Pickard <tom@sliips.com>
2018-03-22 12:37:02 +0000

    no more logging

--- tools/build-scripts/mergeSourceToWorkdir.js
+++ tools/build-scripts/mergeSourceToWorkdir.js
@@ -7,7 +7,7 @@ console.log('starting watch from ' + fromDir + ' to ' + toDir);
 const watcher = require('sync-directory')(fromDir, toDir, {
     watch: true,
     cb({ type, path }) {
-        console.log(type);
-        console.log(path);
+        //console.log(type);
+       //console.log(path);
     }
 });
